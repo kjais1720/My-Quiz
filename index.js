b38efedc1521f1b8421fecde59ba56816d382eda
kjais1720 <66024105+kjais1720@users.noreply.github.com>
2021-08-31 16:05:29 +0000

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var playersName;
-const readline = require('readline-sync');
+const rl = require('readline-sync');
+
+
+var highScores=[{
+  name:"Krituraj",
+  score:5
+  },{
+    name:"Satyam",
+    score:4
+  }];
+
+
 const questions = [
   {
     question:"Where do I live?",
-    answer:"Darbhanga"
+    answer:"Darbhang"
   },
   {
     question:"What is my favourite music genre?",
@@ -12,12 +23,20 @@ const questions = [
   {
     question:"Which is my favourite movie?",
     answer:"Phir Hera Pheri"
+  },
+  {
+    question:"Who is my favourite superhero?",
+    answer:"Shaktiman"
+  },
+  {
+    question:"Who is my favourite singer?",
+    answer:"Shreya Ghosal"
   }
 ]
 
-playersName = readline.question('Who are you?');
-playersName = playersName;
-console.log(`Hey there ${playersName}! \n Welcome to 'DO YOU KNOW KRITURAJ'.`);
+playersName = rl.question('Who are you?');
+console.log(`Hey there ${playersName}!\nWelcome to 'DO YOU KNOW KRITURAJ?'.`);
+console.log("Game starts now. \n----------------")
 play();
 
 
@@ -25,7 +44,7 @@ play();
 var score;
 var answer;
 function askQuestion(quesNum){
-      answer= readline.question(questions[quesNum].question);
+      answer= rl.question(questions[quesNum].question);
         if (questions[quesNum].answer.toUpperCase() === answer.toUpperCase()){
               score+=1;
               console.log('Correct answer');
@@ -38,13 +57,18 @@ function askQuestion(quesNum){
 
 function play(){
   score=0;
-  for (var i=0;i<3;i++){
+  for (var i=0;i<questions.length;i++){
       askQuestion(i);
   }
   console.log(`Yay! Your score is ${score}.\n Thank you for playing!`);
 }
 
-if(readline.keyInYN('Press "y" to play again, "q" to quit.')){
+console.log("The current High scores are:")
+for(const k of highScores){
+  console.log(k.name+" "+k.score);
+}
+
+if(rl.keyInYN('Press "y" to play again, "q" to quit.')){
   play();
 } else{
   console.log("goodbye!!");
